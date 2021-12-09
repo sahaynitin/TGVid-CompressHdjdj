7db6dc74030d3f085cffaff5a7f7e00679d5bf28
Percy <percy@railway.app>
2021-12-09 09:40:12 +0000

    Initial Commit

---
+++ bot/stuff.py
@@ -0,0 +1,25 @@
+import os
+from script import Script
+from pyrogram import Client as Bot, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from .worker import *
+
+
+@Bot.on_message(filters.command(["start"]) & filters.private)
+async def start(bot, update):
+    await update.reply_text(
+        text=Script.START_TEXT.format(update.from_user.mention),
+        disable_web_page_preview=True,
+        reply_markup=Script.START_BUTTONS
+    )
+
+
+async def up(event):
+    if not event.is_private:
+        return
+    stt = dt.now()
+    ed = dt.now()
+    v = ts(int((ed - uptime).seconds) * 1000)
+    ms = (ed - stt).microseconds / 1000
+    p = f"🌋Pɪɴɢ = {ms}ms"
+    await event.reply(v + "\n" + p)
